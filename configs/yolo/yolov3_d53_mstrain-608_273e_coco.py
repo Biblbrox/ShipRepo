1d406efeca8f2225ea16f71955c3ff76191fd00a
Aleksey Kuchkov <biblbroxxx@gmail.com>
2021-08-10 21:25:41 +0300

    Fix path in config

--- configs/yolo/yolov3_d53_mstrain-608_273e_coco.py
+++ configs/yolo/yolov3_d53_mstrain-608_273e_coco.py
@@ -57,7 +57,7 @@ model = dict(
         max_per_img=100))
 # dataset settings
 dataset_type = 'VOCDataset'
-data_root = 'data/SeaShips/'
+data_root = 'data/'
 img_norm_cfg = dict(mean=[0, 0, 0], std=[255., 255., 255.], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile', to_float32=True),
